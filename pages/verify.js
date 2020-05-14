cec805d21416e2f0e8758be0734ce4d1953d21ec
boredomdenied <bchap33@wgu.edu>
2020-05-13 23:11:03 -0500

    tidy up

--- pages/verify.js
+++ pages/verify.js
@@ -1,17 +1,9 @@
 import Head from 'next/head'
 import React, { useState } from 'react'
-import { useRouter } from 'next/router'
 import FormButton from '../components/FormButton'
 import { toast } from 'react-toastify'
-import useSWR from 'swr'
 import { css } from '@emotion/core'
-import RingLoader from 'react-spinners/RingLoader'
 
-const override = css`
-  display: block;
-  margin: 0 auto;
-  // border-color: red;
-`
 class LoginError extends Error {
   constructor({ message, status }) {
     super(message)
@@ -21,60 +13,23 @@ class LoginError extends Error {
 
 export default () => {
   let [email, setEmail] = useState()
-  let [password, setPassword] = useState()
-  const router = useRouter()
 
   const api_host =
     process.env.NODE_ENV === 'production'
       ? 'https://api.byreference.engineer'
       : 'http://localhost:5001'
 
-  const getJson = (url) =>
-    fetch(url, {
-      credentials: 'include',
-      headers: { Accept: 'application/json' },
-    }).then((res) => res.json())
-
-  const { data, error } = useSWR(`${api_host}/api/user/dashboard`, getJson)
-  if (error) {
-    console.error(error)
-  }
-  if (!data)
-    return (
-      <div className="flex p-24 items-center justify-center">
-        <RingLoader
-          css={override}
-          sizeUnit={'px'}
-          size={75}
-          color={'#123abc'}
-        />
-      </div>
-    )
-      if (!data.error) {
-    console.log(data)
-    router.push('/dashboard')
-  }
-
-  const handleForgotPasswordClicked = (e) => {
-    e.preventDefault()
-        router.push('/verify')
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault()
-
     if (!email) toast('Please enter your email', { type: toast.TYPE.INFO })
-    if (!password)
-      toast('Please enter your password', { type: toast.TYPE.INFO })
-
-    fetch(`${api_host}/api/auth/login`, {
+    fetch(`${api_host}/api/user/reset`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
       credentials: 'include',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ email }),
     })
       .then(async (res) => {
         const body = await res.json()
@@ -82,8 +37,9 @@ export default () => {
         if (!res.ok || body.error) {
           throw new LoginError({ status: res.status, message: body.message })
         }
-        // toast('User logged in...', { type: toast.TYPE.SUCCESS })
-        router.push('/dashboard')
+        toast('A verification email has been sent. Check your inbox or spam', {
+          type: toast.TYPE.SUCCESS,
+        })
       })
       .catch((err) => {
         if (err instanceof LoginError) {
@@ -106,37 +62,23 @@ export default () => {
       <form onSubmit={handleSubmit}>
         <div>
           <div className="block text-gray-800 text-2xl font-bold mb-2">
-            Fill out all fields to login
+            Please enter your email
           </div>
           <div className="block text-gray-800 text-lg font-bold mb-2">
             <div className="m-8"></div>
             <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="m-6"></div>
               <label className="block text-gray-700 text-sm font-bold mb-2">
                 Email
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-3 
-              px-4 text-gray-700 leading-tight shadow-outline"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <div className="m-6"></div>
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Password
-              </label>
-              <input type="password"
                 className="shadow appearance-none border rounded w-full
               py-3 px-4 text-gray-700 leading-tight shadow-outline"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <br />
               <div className="p-2 text-center"></div>
               <FormButton />
-              <a
-                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                onClick={handleForgotPasswordClicked}
-              >
-                Forgot Password?
-              </a>
             </div>
           </div>
         </div>
